ee19b163bcd12c02dd2a1bf52643a62b196c5306
Lion2007 <yuriy.gaidai@gmail.com>
2022-03-21 01:53:00 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -1,10 +1,15 @@
+
 var tCtx = document.getElementById('textCanvas').getContext('2d'), //Hidden canvas
     imageElem = document.getElementById('image'); //Image element
   
 // Text input element
 document.getElementById('text').addEventListener('keyup', function() {
+  tCtx.font = "30px Arial";
   tCtx.canvas.width = tCtx.measureText(this.value).width;
-  tCtx.fillText(this.value, 0, 10);
+  tCtx.canvas.height = 30;
+  tCtx.font = "30px Arial";
+  tCtx.fillText(this.value, 0, 30);
   imageElem.src = tCtx.canvas.toDataURL();
   console.log(imageElem.src);
 }, false);
+
